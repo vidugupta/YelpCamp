b999cd095bc2ac21a874a20895ac70fcf213742c
Vidushi Gupta <vidugupta1931@gmail.com>
2024-01-24 21:07:01 +0530

    Add campground create route

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ db.once("open", () => {
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// Parse request body.
+app.use(express.urlencoded({ extended:true }));
+
 app.get('/', (req, res) => {
     res.render('home');
 })
@@ -25,10 +28,20 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', {campgrounds});
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
-} );
+});
 
 app.listen(3000, ()=>{
     console.log('Serving on port 3000')
