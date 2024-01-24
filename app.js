8775b42c905f3971f8e3d44f5d92a868cbab1db7
Vidushi Gupta <vidugupta1931@gmail.com>
2024-01-24 20:46:38 +0530

    Add campground index and show route

--- app.js
+++ app.js
@@ -20,11 +20,15 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: 'My Backyard', description: 'Cheap Camping'});
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
 })
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
+} );
 
 app.listen(3000, ()=>{
     console.log('Serving on port 3000')
