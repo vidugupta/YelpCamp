8b88e594513b2fb0fbb43b707caecf4e2e58f699
Vidushi Gupta <vidugupta1931@gmail.com>
2024-01-25 23:10:08 +0530

    Add campground edit or update route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const app = express();
 const Campground = require('./models/campground')
 
@@ -18,6 +19,8 @@ app.set('views', path.join(__dirname, 'views'))
 
 // Parse request body.
 app.use(express.urlencoded({ extended:true }));
+
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -41,6 +44,17 @@ app.post('/campgrounds', async(req, res) => {
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground});
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(3000, ()=>{
